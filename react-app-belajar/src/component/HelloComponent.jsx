b5a0c9ca22d41a44803e96381b78a04f80bdfd49
xammpp <107601970+xammpp@users.noreply.github.com>
2024-06-12 22:19:33 +0700

    drop and update

--- react-app-belajar/src/component/HelloComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './HelloComponent.css';
-
-const HelloComponent = () => {
-  return <p className="text-p">Hello Fuctional Component</p>;
-};
-
-export default HelloComponent;
--- react-app-belajar/src/container/StateFullComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class StateFullComponent extends React.Component {
-  render() {
-    return <p>StateFull Component Fuctional From DOM</p>;
-  }
-}
-
-export default StateFullComponent;
