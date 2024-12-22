b29457c94d259aec4740a567cea2f12fe2543d77
xammpp <bagusdaffaf01@gmail.com>
2024-12-22 12:20:38 +0700

    add

--- belajar-tailwindcss/tailwind.config.js
+++ belajar-tailwindcss/tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    screens: {
+      laptop: '1024px',
+      // => @media (min-width: 1024px) { ... }
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
